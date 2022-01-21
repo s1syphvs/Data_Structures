64559c94af6e9c0f870392801522a8d5d53c174a
s1syphvs <s1syphvs@protonmail.com>
2022-01-21 16:35:03 -0600

    Add linked_list.h, and create add and delete operations

---
+++ include/linked_list.h
@@ -0,0 +1,30 @@
+/* ========== Operations for linked lists ========== */
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include "structures.h"
+
+/* ----- Add operations ----- */
+
+// At the beggining
+int addNodeStart(NODE **head, NODE **tail, int data);
+
+// At a certain position
+int addNodeMid(NODE **head, NODE **tail, int data, int position);
+
+// At the end
+int addNodeEnd(NODE **head, NODE **tail, int data);
+
+/* ----- Delete operations ----- */
+
+// From the beggining
+int delNodeStart(NODE **head, NODE **tail);
+
+// At a certain position
+int delNodeMid(NODE **head, NODE **tail, int position);
+
+// From the end
+int delNodeEnd(NODE **head, NODE **tail);
+
+#endif
