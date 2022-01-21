01ada89aecf94962f102ea9f3ad26b3c54a96688
s1syphvs <s1syphvs@protonmail.com>
2022-01-21 17:45:32 -0600

    Create addNodeStart function, to add nodes at the beggining of the list

--- src/linked_list.c
+++ src/linked_list.c
@@ -20,3 +20,31 @@ NODE *createNode(int data){
     return(tempNode);
 
 }
+
+/* ----- Add operations ----- */
+
+// At the beggining
+int addNodeStart(NODE **head, NODE **tail, int data){
+
+    NODE *tempNode = createNode(data);
+
+    if(!tempNode)
+        return(EXIT_FAILURE);
+
+    if(*head == *tail && !*head){ // If the list is empty
+        
+        *head = tempNode;
+        *tail = tempNode;
+
+    }
+                   
+    tempNode->nextNode = *head;
+    (*head)->prevNode = tempNode;
+    *head = tempNode;
+
+    if(*head == tempNode)   // Check for errors
+        return(EXIT_SUCCESS);
+    else
+        return(EXIT_FAILURE);
+
+}
